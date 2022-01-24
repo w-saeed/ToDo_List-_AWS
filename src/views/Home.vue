318176c0f1ad041db0f3654e317e907d787002ef
walee <saeed261193@gmail.com>
2022-01-24 14:14:12 +0100

    a commist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <div id="nav">
         <router-link to="/">Log In </router-link> |
 
-        <router-link to="/signup">Sign osjksdjkdsjkdsajkjkdss</router-link>
+        <router-link to="/signup">Sign hello</router-link>
       </div>
 
       <router-view />
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,7 +5,7 @@
       <div id="nav">
         <router-link to="/">Log In </router-link> |
 
-        <router-link to="/signup">Sign Up hello from me</router-link>
+        <router-link to="/signup">Sign Up</router-link>
       </div>
 
       <router-view />

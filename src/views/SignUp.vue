47584b28e391da3504b5eac987175ca209a06ab4
walee <saeed261193@gmail.com>
2022-01-24 03:33:43 +0100

    new commit

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,7 +5,7 @@
       <div id="nav">
         <router-link to="/">Log In </router-link> |
 
-        <router-link to="/signup">Sign Up</router-link>
+        <router-link to="/signup">Sign hello Up</router-link>
       </div>
 
       <router-view />

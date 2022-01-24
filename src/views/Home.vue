42256316ee4106cde4cef4dd598fd6a42f709e98
walee <saeed261193@gmail.com>
2022-01-24 11:37:45 +0100

    nowo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <div id="nav">
         <router-link to="/">Log In </router-link> |
 
-        <router-link to="/signup">Sign Up nowdss </router-link>
+        <router-link to="/signup">Sign osopdijfas</router-link>
       </div>
 
       <router-view />

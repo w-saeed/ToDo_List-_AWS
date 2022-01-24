6df22977736d1cf6e26f9e976ea4c3684a8641b6
walee <saeed261193@gmail.com>
2022-01-24 19:51:23 +0100

    new

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -14,34 +14,37 @@
     <h1>Sign Up</h1>
 
     <br />
+    <form @submit.prevent="submit">
+      <label>Email Address</label><br />
 
-    <label>Email Address</label><br />
+      <input type="email" v-model="email" required />
 
-    <input v-model="email" />
+      <br />
 
-    <br />
+      <label>Username</label><br />
 
-    <label>Username</label><br />
+      <input v-model="userName" required minlength="2" />
 
-    <input v-model="userName" />
+      <br />
 
-    <br />
+      <label>Set Password</label><br />
 
-    <label>Set Password</label><br />
+      <input type="password" v-model="password" required minlength="6" />
 
-    <input type="password" v-model="password" />
+      <br />
 
-    <br />
+      <label>Confirm Password</label><br />
 
-    <label>Confirm Password</label><br />
+      <input type="password" v-model="confirmPassword" required minlength="6" />
 
-    <input type="password" />
+      <br />
+      <p v-show="errors">
+        <b>Password is Wrong or less than 6 characters</b>
+      </p>
 
-    <br />
-
-    <br />
-
-    <button class="btn btn-primary" @click="signup">Signup</button>
+      <br />
+      <button class="button btn btn-primary" type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
@@ -53,44 +56,63 @@ export default {
       userName: "",
       email: "",
       password: "",
+      confirmPassword: "",
+      errors: false,
     };
   },
   methods: {
-    signup() {
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: this.userName,
-          email: this.email,
-          password: this.password,
-        }),
-      };
+    samePassword() {
+      if (
+        this.password == this.confirmPassword &&
+        this.password != "" &&
+        this.confirmPassword != ""
+      ) {
+        this.errors = false;
+      } else {
+        this.errors = true;
+      }
+    },
+    submit() {
+      this.samePassword();
+      if (this.errors == false) {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.userName,
+            email: this.email,
+            password: this.password,
+          }),
+        };
 
-      fetch(this.$store.state.serverUri + "register", requestOptions)
-        .then(async (response) => {
-          const data = await response.json();
+        fetch(this.$store.state.serverUri + "register", requestOptions)
+          .then(async (response) => {
+            const data = await response.json();
 
-          // check for error response
-          if (response.ok) {
-            console.log("register Success");
-            this.$router.push({ name: "Home" });
-          } else {
-            // get error message from body or default to response status
-            console.log("failed to register");
-            const error = (data && data.message) || response.status;
-            return Promise.reject(error);
-          }
-        })
-        .catch((error) => {
-          this.errorMessage = error;
-          console.error("There was an error!", error);
-        });
+            // check for error response
+            if (response.ok) {
+              console.log("register Success");
+              this.$router.push({ name: "Home" });
+            } else {
+              // get error message from body or default to response status
+              console.log("failed to register");
+              const error = (data && data.message) || response.status;
+              return Promise.reject(error);
+            }
+          })
+          .catch((error) => {
+            this.errorMessage = error;
+            console.error("There was an error!", error);
+          });
 
-      //this.$router.push({ name: "Home" });
+        //this.$router.push({ name: "Home" });
+      }
+      if (this.errors == true) {
+        this.errors = true;
+      }
     },
   },
 };

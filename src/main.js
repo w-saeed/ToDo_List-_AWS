2021eb42f0ef1385bfc2c2eca2a112b6ab9a5c53
walee <saeed261193@gmail.com>
2022-01-24 13:07:06 +0100

    a commit

--- src/main.js
+++ src/main.js
@@ -3,15 +3,22 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import Vuelidate from "vuelidate"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+
 import router from './router'
 
 import store from "./store";
 
 
+
+
 Vue.config.productionTip = false
+
+
+Vue.use(Vuelidate)
 
 Vue.use(router)
 
